471835bac4f3ae013371476ee4c89b3145dfb2cc
nimfersus <105855729+nimfersus@users.noreply.github.com>
2022-05-19 14:05:19 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 2) {
+        basic.setLedColor(0xffff00)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(74)
 basic.forever(function () {
-	
+    basic.pause(100)
+    basic.turnRgbLedOff()
 })
